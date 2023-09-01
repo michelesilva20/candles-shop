872de2377b3f1691f7a63994e284d2050b40084a
michelesilva20 <michele.silva0511@gmail.com>
2023-09-01 20:14:00 -0300

    footer and feedback

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <>
+      <footer>
+        <div className='d-flex justify-content-between container align-items-center'>
+          <div>
+            <img src={logo} alt="logo oficial" />
+          </div>
+          <div>
+            <p>E-mail: <a href="#">xxxxx@candleshop.com.br</a></p>
+            <p>Telefone: (00) 00000-0000</p>
+            <p>CNPJ: XX. XXX. XXX/0001-XX.</p>
+          </div>
+          <div className='d-flex flex-column'>
+            <a className='mb-2' href="#">Política de Privacidade</a>
+            <a href="#">Termos de uso</a>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
--- src/components/home/Feedback.jsx
+++ src/components/home/Feedback.jsx
@@ -1,44 +1,122 @@
 import React from 'react'
 import feedback from '../../assets/feedback.jpeg'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const Feedback = () => {
   return (
     <>
       <section className="container d-flex flex-wrap justify-content-between">
-        <div className='card-feedback d-flex justify-content-center flex-column'>
-          <div className='d-flex align-items-center'>
-            <img src={feedback} alt="avatar" />
-            <div className='ms-3'>
-              <p className='name'>Aline de Castro</p>
-              <p className='age'>26 anos</p>
-              <div className='d-flex'>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className='card-feedback d-flex justify-content-center flex-column'>
+            <div className='d-flex align-items-center'>
+              <img src={feedback} alt="avatar" />
+              <div className='ms-3'>
+                <p className='name'>Aline de Castro</p>
+                <p className='age'>26 anos</p>
+                <div className='d-flex'>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                </div>
               </div>
             </div>
+            <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
           </div>
-          <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-        </div>
-        <div className='card-feedback d-flex justify-content-center flex-column'>
-          <div className='d-flex align-items-center'>
-            <img src={feedback} alt="avatar" />
-            <div className='ms-3'>
-              <p className='name'>Aline de Castro</p>
-              <p className='age'>26 anos</p>
-              <div className='d-flex'>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
-                <i class='bx bxs-star'></i>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='card-feedback d-flex justify-content-center flex-column'>
+            <div className='d-flex align-items-center'>
+              <img src={feedback} alt="avatar" />
+              <div className='ms-3'>
+                <p className='name'>Aline de Castro</p>
+                <p className='age'>26 anos</p>
+                <div className='d-flex'>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                </div>
               </div>
             </div>
+            <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
           </div>
-          <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='card-feedback d-flex justify-content-center flex-column'>
+            <div className='d-flex align-items-center'>
+              <img src={feedback} alt="avatar" />
+              <div className='ms-3'>
+                <p className='name'>Aline de Castro</p>
+                <p className='age'>26 anos</p>
+                <div className='d-flex'>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                </div>
+              </div>
+            </div>
+            <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='card-feedback d-flex justify-content-center flex-column'>
+            <div className='d-flex align-items-center'>
+              <img src={feedback} alt="avatar" />
+              <div className='ms-3'>
+                <p className='name'>Aline de Castro</p>
+                <p className='age'>26 anos</p>
+                <div className='d-flex'>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                  <i class='bx bxs-star'></i>
+                </div>
+              </div>
+            </div>
+            <p className='mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          </div>
+        </SwiperSlide>
+
+        
+      </Swiper>
+      
+        
       </section>
     </>
   )
